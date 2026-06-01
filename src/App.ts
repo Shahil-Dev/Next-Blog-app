df9aa5c82a4c6ac67a3f9a1dcb7cc7a87c49507b
Shahil-Dev <alfee9006@gmail.com>
2026-06-01 23:45:09 +0600

    Implementing Email Verification

--- src/App.ts
+++ src/App.ts
@@ -2,11 +2,17 @@ import express from "express";
 import { PostRoutes } from "./modules/Post/post.route";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-
+import cors from "cors";
 const app = express();
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/posts", PostRoutes);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,40 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { stat } from "node:fs";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-     emailAndPassword: { 
-    enabled: true, 
-  }, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        default: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        default: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("*********verification email successfully**********");
+    },
+  },
 });
