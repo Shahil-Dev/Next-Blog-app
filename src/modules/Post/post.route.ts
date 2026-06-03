8bd43376eed8232ed3d0bddfe6dfe14cdfe59af3
Shahil-Dev <alfee9006@gmail.com>
2026-06-04 00:50:56 +0600

    Authentication Middleware

--- src/modules/Post/post.route.ts
+++ src/modules/Post/post.route.ts
@@ -1,8 +1,18 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { PostController } from "./post.controller";
+import { auth } from "../../lib/auth";
 
 const router = express.Router();
 
-router.post("/", PostController.createPost);
+const authMiddleware = (...role: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const session = await auth.api.getSession(
+        { headers: req.headers as any }
+    );
+    console.log(session)
+  };
+};
+
+router.post("/", authMiddleware("Admin"), PostController.createPost);
 
 export const PostRoutes = router;
