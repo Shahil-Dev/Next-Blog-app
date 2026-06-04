c152b816c540ae0e96ca037ace3228171f74f67b
Shahil-Dev <alfee9006@gmail.com>
2026-06-05 02:34:00 +0600

    Advanced Boolean Filtering Techniques

--- src/modules/Post/post.controller.ts
+++ src/modules/Post/post.controller.ts
@@ -21,8 +21,9 @@ const getAllPosts = async (req: Request, res: Response) => {
     const searchString = typeof search === "string" ? search : undefined;
     const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
     const isFeatured = req.query.isFeatured
-      ? req.query.isFeatured === "true"
-      : false;
+      ? req.query.isFeatured === "true"?true
+      : req.query.isFeatured === "false"?false: undefined
+      :  undefined;
 
     console.log({isFeatured});
 
--- src/modules/Post/post.service.ts
+++ src/modules/Post/post.service.ts
@@ -22,7 +22,7 @@ const getAllPosts = async ({
 }: {
   search: string | undefined;
   tags: string[] | [];
-  isFeatured: boolean 
+  isFeatured: boolean  | undefined;
 }) => {
   const andConditions: PostWhereInput[] = [];
   if (search) {
