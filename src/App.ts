6d9221346960f2585c15aa13fabe3e8160662c06
Shahil-Dev <alfee9006@gmail.com>
2026-06-15 12:37:42 +0600

    Debuging part-1

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,15 @@
 import express from "express";
-import { PostRoutes } from "./modules/Post/post.route";
+import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-import cors from "cors";
+import { PostRoutes } from "./modules/Post/post.route";
 import { CommentRoutes } from "./modules/Comment/comment.route";
 import { ReactionRoutes } from "./modules/Reaction/reaction.route";
+import { FriendRequestRoutes } from "./modules/FriendRequest/friendRequest.route";
 import globalErrorHandler from "./Middleware/globalErrorHandler";
 import { notFound } from "./Middleware/notFound";
-import { FriendRequestRoutes } from "./modules/FriendRequest/friendRequest.route";
-const app = express();
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+const app = express();
 
 app.use(
   cors({
@@ -20,14 +19,17 @@ app.use(
 );
 app.use(express.json());
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use("/posts", PostRoutes);
 app.use("/comments", CommentRoutes);
 app.use("/reacts", ReactionRoutes);
 app.use("/friends", FriendRequestRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! Backend Server is running successfully.");
 });
+
 app.use(notFound);
 app.use(globalErrorHandler);
 
