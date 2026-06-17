ea6121831005373507674857eec7824d82c8a50e
Shahil-Dev <alfee9006@gmail.com>
2026-06-17 09:31:01 +0600

    fix: include prisma generated files for vercel

--- src/Server.ts
+++ src/Server.ts
@@ -1,21 +1,25 @@
-import app from "./App";
+import app from "./App.js";
 import { prisma } from "./lib/prisma.js";
 
 const PORT = process.env.PORT || 3000;
 
-async function Server() {
-  try {
-    await prisma.$connect();
-    console.log("Prisma Connected the database");
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error);
-    await prisma.$disconnect();
-    process.exit(1);
+if (process.env.NODE_ENV !== "production") {
+  async function startServer() {
+    try {
+      await prisma.$connect();
+      console.log("Prisma Connected the database");
+      app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+      });
+    } catch (error) {
+      console.error("Error starting the server:", error);
+      await prisma.$disconnect();
+      process.exit(1);
+    }
   }
+  startServer();
 }
 
-Server();
+
+export default app;
